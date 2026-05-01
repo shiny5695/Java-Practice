875ff64e7b1ebe01f12e259e1de0361194200e57
Shiny Ingles <shine.561995@gmail.com>
2026-05-02 00:17:35 +0530

    ParallelStreams related

---
+++ streams/UseCaseOfParallelStreams.java
@@ -0,0 +1,31 @@
+package streams;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+//currently not working but it should
+public class UseCaseOfParallelStreams {
+
+	public static void main(String[] args) {
+		
+        List<Integer> list = new ArrayList<>();
+        for(int i = 0; i < 100000000; i++) {
+        	list.add(i);
+        }
+        
+        long startTime = System.currentTimeMillis();
+        list.parallelStream().map(element->heavyOperations(element)).collect(Collectors.toList());
+        long endTime = System.currentTimeMillis();
+        System.out.println(endTime-startTime);
+
+	}
+	
+	private static int heavyOperations(int i) {
+		for(int j = 0; i <1000; j++) {
+			i = j+i;
+		}
+		return i;
+	}
+
+}
