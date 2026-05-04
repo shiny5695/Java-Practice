d0c37ecbb07beb65f5cc31e7400cd3378df1fa1e
Shiny Ingles <shine.561995@gmail.com>
2026-05-04 23:57:52 +0530

    Added repositories for AtmCard

---
+++ systemDesign/AtmCardRepositoryImpl.java
@@ -0,0 +1,29 @@
+package systemDesign;
+
+public class AtmCardRepositoryImpl implements AtmCardRepository {
+
+	@Override
+	public void insertCard() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public double checkBalance() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double withDrawCash() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean changePin() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
